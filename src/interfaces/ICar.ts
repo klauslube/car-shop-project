18f6cc35f8e9a672b5a233aa815e82170324dcb3
klauslube <klauslube93@gmail.com>
2022-10-21 19:06:02 -0300

    fix: correçao do numero de portas

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 import { VehicleZodSchema } from './IVehicle';
 
 const CarZodSchema = VehicleZodSchema.extend({
-  doorsQty: z.number().min(4).int(),
+  doorsQty: z.number().gte(2).lte(4).int(),
   seatsQty: z.number().gte(2).lte(7),
 });
 
