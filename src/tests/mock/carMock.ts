1a879c6523c1a1442e37b1ad438f493f1c1f9dcc
klauslube <klauslube93@gmail.com>
2022-10-21 19:06:53 -0300

    feat: criaçao de carMock

---
+++ src/tests/mock/carMock.ts
@@ -0,0 +1,41 @@
+import {ICar} from '../../interfaces/ICar'
+
+const carMock:ICar = {
+  model: "Ferrari Maranello",
+  year: 1963,
+  color: "red",
+  buyValue: 3500000,
+  seatsQty: 4,
+  doorsQty: 4
+};
+
+const carMockWithId:ICar & { _id:string } = {
+  _id: '62cf1fc6498565d94eba52cd',
+  model: "Ferrari Maranello",
+  year: 1963,
+  color: "red",
+  buyValue: 3500000,
+  seatsQty: 4,
+  doorsQty: 4
+};
+
+const carArrayMock:ICar[] = [
+  {
+    model: "Ferrari Maranello",
+    year: 1963,
+    color: "red",
+    buyValue: 3500000,
+    seatsQty: 4,
+    doorsQty: 4
+  },
+  {
+    model: "Ford Fusion",
+    year: 2012,
+    color: "silver",
+    buyValue: 1500000,
+    seatsQty: 5,
+    doorsQty: 4
+  },
+]
+
+export { carMock, carMockWithId, carArrayMock };
