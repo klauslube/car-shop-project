ac7f942db611cd7d49aad0b651e793c519ce92b6
klauslube <klauslube93@gmail.com>
2022-10-21 17:25:11 -0300

    feat: criado CarService

---
+++ src/services/CarService.ts
@@ -0,0 +1,49 @@
+import { IService } from '../interfaces/IService';
+import { ICar, CarZodSchema } from '../interfaces/ICar';
+import { IModel } from '../interfaces/IModel';
+import { ErrorTypes } from '../errors/catalog';
+
+export default class CarService implements IService<ICar> {
+  private _car:IModel<ICar>;
+
+  constructor(model:IModel<ICar>) {
+    this._car = model;
+  }
+
+  public async create(obj:unknown):Promise<ICar> {
+    const parsed = CarZodSchema.safeParse(obj);
+
+    if (!parsed.success) {
+      throw parsed.error;
+    }
+    return this._car.create(parsed.data);
+  }
+
+  public async readOne(_id:string):Promise<ICar> {
+    const car = await this._car.readOne(_id);
+    if (!car) throw new Error(ErrorTypes.EntityNotFound);
+    return car;
+  }
+
+  public async read():Promise<ICar[]> {
+    const car = await this._car.read();
+    if (!car) throw new Error(ErrorTypes.EntityNotFound);
+    return car;
+  }
+
+  public async update(_id:string, obj:unknown):Promise<ICar> {
+    const parsed = CarZodSchema.safeParse(obj);
+    if (!parsed.success) {
+      throw parsed.error;
+    }
+    const car = await this._car.update(_id, parsed.data);
+    if (!car) throw new Error(ErrorTypes.EntityNotFound);
+    return car;
+  }
+
+  public async delete(_id:string):Promise<ICar> {
+    const deletedCar = await this._car.delete(_id);
+    if (!deletedCar) throw new Error(ErrorTypes.EntityNotFound);
+    return deletedCar;
+  }
+}
