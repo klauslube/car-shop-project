5a83a0e253c14ce63b6e49218dea5e2a85f46379
klauslube <klauslube93@gmail.com>
2022-10-21 17:23:01 -0300

    feat: criado app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 import express from 'express';
+import errorHandler from './middlewares/errors';
+import carRouter from './routes/cars';
 
 const app = express();
- 
+
+app.use(carRouter);
+app.use(errorHandler);
+
 export default app;
