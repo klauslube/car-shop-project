7caedefeb09a76989e7be527adb92562d49fe672
klauslube <klauslube93@gmail.com>
2022-10-21 18:20:36 -0300

    feat: inserido o express.json

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import errorHandler from './middlewares/errors';
 import carRouter from './routes/cars';
 
 const app = express();
-
+app.use(express.json());
 app.use(carRouter);
 app.use(errorHandler);
 
