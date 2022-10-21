dd983e8bcc4840750b8fd401bfddf1994f0cced6
klauslube <klauslube93@gmail.com>
2022-10-21 19:41:20 -0300

    test: caso de sucesso do metodo read

--- src/tests/unit/models/carModel.test.ts
+++ src/tests/unit/models/carModel.test.ts
@@ -3,11 +3,14 @@ import chai from 'chai';
 const { expect } = chai;
 import CarModel from '../../../models/CarModel'
 import { Model } from 'mongoose';
-import { carMock, carMockWithId } from '../../mock/carMock';
+import { ICar } from '../../../interfaces/ICar';
+import { carArrayMock, carMock, carMockWithId } from '../../mock/carMock';
 describe('Car Model', () => {
   const carModel = new CarModel()
   before(async () => {
     sinon.stub(Model, 'create').resolves(carMockWithId)
+    sinon.stub(Model, 'find').resolves(carArrayMock)
+    
 
   });
 
@@ -19,6 +22,17 @@ describe('Car Model', () => {
 		it('successfully created', async () => {
 			const newCar = await carModel.create(carMock);
 			expect(newCar).to.be.deep.equal(carMockWithId);
+		});
+	});
+
+  describe('searching all cars', () => {
+		it('successfully found', async () => {
+			const carFound = await carModel.read();
+			expect(carFound).to.be.an('array')
+			carFound?.forEach((car: ICar, index: number) => {
+				expect(car).to.be.deep.equal(carArrayMock[index]);
+			})
+			expect(carFound?.length).to.be.gt(1)
 		});
 	});
 
