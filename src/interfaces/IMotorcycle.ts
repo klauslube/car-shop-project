122e6e0364d85585975c2f91b2d08700c54a3feb
klauslube <klauslube93@gmail.com>
2022-10-24 19:19:35 -0300

    refactor: alterado tipo de category para um enum

--- src/interfaces/IMotorcycle.ts
+++ src/interfaces/IMotorcycle.ts
@@ -2,7 +2,11 @@ import { z } from 'zod';
 import { VehicleZodSchema } from './IVehicle';
 
 const MotorCycleZodSchema = VehicleZodSchema.extend({
-  category: z.string(),
+  category: z.enum([
+    'Street',
+    'Custom',
+    'Trail',
+  ]),
   engineCapacity: z.number().lte(2500).int().positive(),
 });
 
